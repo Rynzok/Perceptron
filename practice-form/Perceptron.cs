b3db58a92f0d01f90cc167ce44dcc183db03e32d
Виктор Киселёв <vkisel102@yandex.ru>
2023-04-29 15:22:28 +0300

    Мелкие изменения

--- practice-form/Perceptron.cs
+++ practice-form/Perceptron.cs
@@ -24,19 +24,18 @@ namespace practice_form
         public static void training()
         {
             weights = new int[4, 25];
-            //Genetic_Algoritm.Main();
             int N = 100;
             Population population = new Population(N, 0);
+
             // Цифры (Обучающая выборка)
-            var n0 = "0010001010100010000000000".ToCharArray();
-            var n1 = "0000000000100010101000100".ToCharArray();
-            var n2 = "0010000010111110001000100".ToCharArray();
-            var n3 = "0010000100001000000000100".ToCharArray();
+            var n0 = "0010001010100010000000000".ToCharArray(); // ˄
+            var n1 = "0000000000100010101000100".ToCharArray(); // ˅
+            var n2 = "0010000010111110001000100".ToCharArray(); // →
+            var n3 = "0010000100001000000000100".ToCharArray(); // !
 
 
             // Список всех вышеуказанных цифр
             char[][] nums = { n0, n1, n2, n3 };
-            //Char[] symbols = new char[4]{ '˄', '˅', '→', '!'};
 
             // Тренировка сети
             for (int n = 0; n <4; n++)
@@ -82,8 +81,6 @@ namespace practice_form
             {
                 net += int.Parse(base_number[i].ToString()) * weights[i];
             }
-            
-
             // Возвращаем разницу
             return net;
         }
@@ -100,33 +97,5 @@ namespace practice_form
             // Превышен ли порог? (Да - сеть думает, что это 5. Нет - сеть думает, что это другая цифра)
             return net >= bias;
         }
-
-        // Уменьшение значений весов, если сеть ошиблась и выдала 1
-        //private static void decrease(char[] number, int rightNum)
-        //{
-        //    for (int i = 0; i < 25; i++)
-        //    {
-        //        // Возбужденный ли вход
-        //        if (int.Parse(number[i].ToString()) == 1)
-        //        {
-        //            // Уменьшаем связанный с ним вес на единицу
-        //            weights[rightNum, i]--;
-        //        }
-        //    }
-        //}
-
-        // Увеличение значений весов, если сеть ошиблась и выдала 0
-        //private static void increase(char[] number, int rightNum)
-        //{
-        //    for (int i = 0; i < 25; i++)
-        //    {
-        //        // Возбужденный ли вход
-        //        if (int.Parse(number[i].ToString()) == 1)
-        //        {
-        //            // Увеличиваем связанный с ним вес на единицу
-        //            weights[rightNum, i]++;
-        //        }
-        //    }
-        //}
     }
 }
